9f7beec78d1c0addb5462222b32696e04f51bcd9
pooja maheshwari <pooja.chugh15@gmail.com>
2021-06-21 15:01:30 +0100

    test passed

--- cypress/integration/titlebar.spec.js
+++ cypress/integration/titlebar.spec.js
@@ -4,7 +4,7 @@ describe('TitleBar', () => {
   });
 
   it('should have a title', () => {
-    cy.get('h1').should('contain', 'Hit Parade');
+    cy.get('h1').should('contain', 'Poojas Hit Parade');
   });
 
   it('should have a select populated with genres', () => {
